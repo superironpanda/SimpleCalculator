e0c6ddb340dd21172a4dc3abd92d92ae917e5ba9
root <superironpanda@gmail.com>
2019-10-29 03:24:55 +0000

    Added main function

--- src/src/main/Main.java
+++ src/src/main/Main.java
@@ -7,7 +7,29 @@ import com.sendError.SimpleCalculator.*;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		boolean noExit = true;
+		while(noExit) {
+			Prompt prompt = new Prompt();
+			UserInput userinput = new UserInput();
+			try {
+				userinput.getOperationChoice();
+				userinput.setOperation();
+
+				if(!userinput.exit()) {
+					userinput.getTwoNumbers();
+					prompt.result = userinput.operation.numberOperations();
+					prompt.displayResult();
+					if(userinput.ifContinue())
+						noExit = true;
+					else
+						noExit = false;
+				}	
+			} catch (Exception e) {
+				SendError error = new SendError();
+				error.wrongTypeOfInput();
+			}
+			
+		}
 	}
 	
 }
